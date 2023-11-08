8eb6916a3154599cccf26d71e264ef9eb730f912
RussMaxwell <52090077+RussMaxwell@users.noreply.github.com>
2023-11-08 16:50:25 -0500

    Delete JoansPizza directory

--- JoansPizza/src/data.js
+++
@@ -1,7 +0,0 @@
-import { nanoid } from "nanoid";
-
-export const links = [
-  { id: nanoid(), href: "#home", text: "home" },
-  { id: nanoid(), href: "#menu", text: "menu" },
-  { id: nanoid(), href: "#aboutus", text: "about us" },
-];
